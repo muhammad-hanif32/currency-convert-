687b151d174e6ba9218decff7accb370370c6fed
muhammad-hanif32 <mh119974@gmail.com>
2024-04-27 22:21:19 -0700

    push my currency convert to github

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.94,
+    GBP: 0.80,
+    INR: 83.29,
+    PKR: 277.31,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: 'Enter Your Currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'Amount',
+        message: 'Enter Your Amount',
+        type: 'number'
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let Amount = user_answer.Amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
